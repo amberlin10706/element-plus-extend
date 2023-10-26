1d6eab9c920edbc05b70f5be0b32bbbcef8d545f
amberlin10706 <yyyy2248@hotmail.com>
2023-10-26 17:39:47 +0800

    AppEnterInput

---
+++ src/components/AppEnterInput.vue
@@ -0,0 +1,21 @@
+<script setup>
+import { ref } from 'vue'
+
+const emits = defineEmits(['enter'])
+
+const isComposing = ref(false)
+const handleEnter = () => {
+  if (isComposing.value) return
+  emits('enter')
+}
+</script>
+
+<template>
+  <el-input
+    @keydown.enter="handleEnter"
+    @compositionstart="() => (isComposing = true)"
+    @compositionend="() => (isComposing = false)"
+  ></el-input>
+</template>
+
+<style scoped lang="scss"></style>

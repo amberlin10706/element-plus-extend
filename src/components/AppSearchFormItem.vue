a53b25a31e3a1da4864c65638ea8214eeb709181
amberlin10706 <yyyy2248@hotmail.com>
2024-07-29 20:27:33 +0800

    AppSearchFormItem and AppTableColumn

---
+++ src/components/AppSearchFormItem.vue
@@ -0,0 +1,45 @@
+<script setup>
+const props = defineProps({
+  type: {
+    type: String,
+    default: 'text',
+  },
+  title: {
+    type: String,
+    default: '',
+  },
+  itemKey: {
+    type: String,
+    default: '',
+  },
+  form: {
+    type: Object,
+    required: true,
+  },
+  options: {
+    type: Array,
+    default: () => [],
+  },
+})
+
+const value = toRef(props.form, props.itemKey)
+</script>
+
+<template>
+  <el-form-item v-if="type === 'text'" :label="title" :prop="itemKey">
+    <el-input v-model="value" :placeholder="`輸入${title}`" />
+  </el-form-item>
+
+  <el-form-item v-if="type === 'select'" :label="title" :prop="itemKey">
+    <el-select v-model="value" :placeholder="`選擇${title}`">
+      <el-option
+        v-for="item in options"
+        :key="item.value"
+        :label="item.label"
+        :value="item.value"
+      />
+    </el-select>
+  </el-form-item>
+</template>
+
+<style scoped lang="scss"></style>
---
+++ src/components/AppTableColumn.vue
@@ -0,0 +1,34 @@
+<script setup>
+defineProps({
+  label: {
+    type: String,
+    default: '',
+  },
+  prop: {
+    type: String,
+    default: '',
+  },
+  formatter: {
+    type: Function,
+    default: (row, column, cellValue) => cellValue,
+  },
+  minWidth: {
+    type: Number,
+    default: 0,
+  },
+  component: {
+    type: Object,
+    default: null,
+  },
+})
+</script>
+
+<template>
+  <el-table-column :label="label" :prop="prop" :formatter="formatter" :min-width="minWidth">
+    <template v-if="component" #default="{ row, column }">
+      <component :is="component" :row="row" :column="column" />
+    </template>
+  </el-table-column>
+</template>
+
+<style scoped lang="scss"></style>
